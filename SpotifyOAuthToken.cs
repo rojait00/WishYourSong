e426ec8b31ce84a63958b324965f7af56868493d
Robin Jahke <robin.jahke@adesso.de>
2022-06-22 22:31:28 +0200

    Projektdateien hinzufügen.

---
+++ SpotifyOAuthToken.cs
@@ -0,0 +1,12 @@
+﻿namespace WishYourSong
+{
+    public class SpotifyOAuthToken
+    {
+        /// <summary>
+        /// To Do: Replace with your own Token
+        /// The fastest way to get the token is by going on this page and koicking "GET TOKEN"
+        /// https://developer.spotify.com/console/get-playlist-tracks/
+        /// </summary>
+        public const string Token = "";
+    }
+}
