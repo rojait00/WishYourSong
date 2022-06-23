77e0e42ecbe1140357cbe9a9a53c55175f15fa00
Robin Jahke <robin.jahke@adesso.de>
2022-06-23 19:23:45 +0200

    First Version multiple Votes allowed

---
+++ Data/SongDatabase.cs
@@ -0,0 +1,34 @@
+﻿using SpotifyAPI.Web;
+
+namespace WishYourSong.Data
+{
+    public class SongDatabase
+    {
+        public List<FullTrack> Tracks { get; set; } = new List<FullTrack>();
+
+        public void AddTrack(FullTrack track)
+        {
+            track.Popularity = 1;
+            Tracks.Add(track);
+        }
+
+        public async Task<List<FullTrack>> SearchTrackAsync(string term)
+        {
+            var spotify = new SpotifyClient(SpotifyTokens.OAuth);
+
+            var request = new SearchRequest(SearchRequest.Types.Track, term)
+            {
+                Limit = 50,
+                Market = "DE"
+            };
+
+            var items = await spotify.Search.Item(request);
+            return items?.Tracks?.Items ?? new List<FullTrack>();
+        }
+
+        public List<FullTrack> GetContainedTracks(List<FullTrack> result)
+        {
+            return result.Where(x => Tracks.Any(dbT => dbT.Id == x.Id)).ToList();
+        }
+    }
+}
--- SpotifyTokens.cs
+++ SpotifyTokens.cs
@@ -1,14 +1,26 @@
-﻿namespace WishYourSong
+﻿using SpotifyAPI.Web;
+
+namespace WishYourSong
 {
     public class SpotifyTokens
     {
         /// <summary>
-        /// To Do: Replace with your own Token
-        /// The fastest way to get the token is by going on this page and koicking "GET TOKEN"
-        /// https://developer.spotify.com/console/get-playlist-tracks/
+        /// To Do: Replace with your own values
+        /// Follow this instructions to get the credentials and create the specified files
+        /// https://johnnycrazy.github.io/SpotifyAPI-NET/docs/auth_introduction
+        /// 
+        /// Create App here: https://developer.spotify.com/dashboard/
         /// </summary>
-        public const string Token = "";
+        public static SpotifyClientConfig OAuth { get { return GetOAuth(); } }
 
+        private static SpotifyClientConfig GetOAuth()
+        {
+            string id = File.ReadAllText(@"C:\WishYourSong\clientId.txt").Trim();
+            string pass = File.ReadAllText(@"C:\WishYourSong\clientSecret.txt").Trim();
 
+            return SpotifyClientConfig
+                .CreateDefault()
+                .WithAuthenticator(new ClientCredentialsAuthenticator(id, pass));
+        }
     }
 }
